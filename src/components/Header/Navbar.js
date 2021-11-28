c772b2e2805e12787fdc934168afa3ab08c322f5
sabuhitapdigli <sabuhitapdigli@gmail.com>
2021-11-28 12:28:26 +0400

    first test

---
+++ src/components/Header/Navbar.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+const Navbar = () => {
+    return(
+        <Nav>
+
+        </Nav>
+    )
+}   
+
+const Nav = styled.div`
+    
+`
+export default Navbar
